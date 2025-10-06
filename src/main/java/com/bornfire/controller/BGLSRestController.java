955b6a5a0535c49d2000f3878be6b89a2a7a5505
Aishwarya <160707726+Aishuvenky@users.noreply.github.com>
2025-10-06 13:32:12 +0530

    rest control update

--- src/main/java/com/bornfire/controller/BGLSRestController.java
+++ src/main/java/com/bornfire/controller/BGLSRestController.java
@@ -9389,6 +9389,7 @@ public class BGLSRestController {
 			map.put("id", row[1]); // l.ID
 			map.put("account_holderkey", row[3]); // adjust index to match your query
 			map.put("loan_name", row[12]);
+			map.put("assigned_branchkey", row[14]);
 			map.put("retailer_name", row[64]);
 			map.put("retailer_branch", row[65]);
 			map.put("account_state", row[9]);
@@ -9434,6 +9435,7 @@ public class BGLSRestController {
 			map.put("id", row[1]); // l.ID
 			map.put("account_holderkey", row[3]); // adjust index to match your query
 			map.put("loan_name", row[12]);
+			map.put("assigned_branchkey", row[14]);
 			map.put("retailer_name", row[64]);
 			map.put("retailer_branch", row[65]);
 			map.put("account_state", row[9]);
